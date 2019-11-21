a927af5206590a0dcc358e7dc113b5a4041f92fa
skyRain <506998345@qq.com>
2019-11-21 11:05:07 +0800

    extract github config from code to properties

--- src/main/java/com/skyrain/community/controller/AuthorizeController.java
+++ src/main/java/com/skyrain/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.skyrain.community.dto.AccessTokenDTO;
 import com.skyrain.community.dto.GithubUser;
 import com.skyrain.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
                            @RequestParam("state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("25b3472a244be3dad453");
-        accessTokenDTO.setClient_secret("46730fd8311bc72f0b3b3c8b9e4d122f3b858803");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println(accessToken);
